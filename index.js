51e8cee24a9c13358b36421928b4228d1eb1f6f7
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-11-28 13:12:12 -0500

    pushing final code

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const compose = require('./utils/compose');
 
 const startVal = 4;
 
-const squared = x => x ** x;
+const squared = x => x * x;
 const doubled = x => x * 2;
 const addTen = x => x + 10;
 const halfNum = x => x;
 
+const trace = label => value => {
+    console.log(`${label}: ${value}`)
+    return value;
+};
+
+
 const result = compose(
+    trace('after halfnum'),
     halfNum,
+    trace('after addTen'),
     addTen,
+    trace('after doubled'),
     doubled,
+    trace('after squared'),
     squared
 )(startVal)
 
