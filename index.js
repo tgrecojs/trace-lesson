5cf3c503e11df2eb22b7d4f31806addcd6e7ce88
Thomas Greco <tgreco4@fordham.edu>
2017-11-28 19:00:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const startVal = 4;
 const squared = x => x * x;
 const doubled = x => x * 2;
 const addTen = x => x + 10;
-const halfNum = x => x;
+const halfNum = x => x / 2;
 
 const trace = label => value => {
     console.log(`${label}: ${value}`)
